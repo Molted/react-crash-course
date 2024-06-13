1c294715b7fc804ad75362a17f36d0fae830ccc6
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2024-06-13 17:59:45 +0800

    Add New Post Component and useState

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
+import NewPost from "./NewPost";
 import Post from "./Post";
 import styles from "./css/PostsList.module.css";
 
 function PostsList() {
+	const [enteredBody, setEnteredBody] = useState("");
+	const [enteredAuthor, setEnteredAuthor] = useState("");
+
+	function changeBodyHandler(event: any) {
+		setEnteredBody(event.target.value);
+	}
+
+	function changeAuthorHandler(event: any) {
+		setEnteredAuthor(event.target.value);
+	}
+
 	return (
-		<ul className={styles.posts}>
-			<Post author="Testing" body="React.js is Awesome" />
-			<Post author="Testing Again" body="Check out the full course!" />
-		</ul>
+		<>
+			<NewPost
+				onBodyChange={changeBodyHandler}
+				onAuthorChange={changeAuthorHandler}
+			/>
+			<ul className={styles.posts}>
+				<Post author={enteredAuthor} body={enteredBody} />
+				<Post author="Testing Again" body="Check out the full course!" />
+			</ul>
+		</>
 	);
 }
 
